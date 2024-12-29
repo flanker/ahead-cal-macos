31b262cab79a939f7e5ab7b7795353a3e56871b6
FENG Zhichao <flankerfc@gmail.com>
2024-12-29 21:34:32 +0800

    Initial Commit

---
+++ AheadCal/AheadCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AheadCalApp.swift
+//  AheadCal
+//
+//  Created by Zhichao Feng on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct AheadCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
