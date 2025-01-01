6a7b0a81705a5f067800e5a2f749f65f8fbe84cf
FENG Zhichao <flankerfc@gmail.com>
2025-01-01 09:05:24 +0800

    init widget target

---
+++ AheadCalWidget/AheadCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AheadCalWidgetBundle.swift
+//  AheadCalWidget
+//
+//  Created by Zhichao Feng on 2025/1/1.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AheadCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AheadCalWidget()
+    }
+}
---
+++ AheadCalWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  AheadCalWidget
+//
+//  Created by Zhichao Feng on 2025/1/1.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
